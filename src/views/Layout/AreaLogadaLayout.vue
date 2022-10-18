bd065f7ea8728c6703222c834040e3ae19b2b2f0
Marcos Osorio <marcos2106@hotmail.com>
2022-10-18 14:11:27 -0300

    correção menu apostar

--- src/views/Layout/AreaLogadaLayout.vue
+++ src/views/Layout/AreaLogadaLayout.vue
@@ -112,7 +112,7 @@ export default {
 		},
         verificaApostaFinalizada() {
             this.$clubApi.get("/bolao/finalizada").then((response) => {
-                this.finalizada = false;//response.data.object;
+                this.finalizada = response.data.object;
             })
         }
 	}
