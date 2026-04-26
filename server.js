86deb93f895b8c4d77a3c7cdddccfda0fdbe6fe4
Marcos Osorio <marcos2106@hotmail.com>
2026-04-26 13:25:00 -0300

    criar uma rota de bolao10 para www.bolao10

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// Middleware para redirecionar bolao10.net -> www.bolao10.net
+app.use((req, res, next) => {
+  if (req.headers.host === 'bolao10.net') {
+    return res.redirect(301, 'https://www.bolao10.net' + req.url)
+  }
+  next()
+})
+
 // Middleware para servir os arquivos da pasta dist
 app.use(express.static(path.join(__dirname, 'dist')))
 
